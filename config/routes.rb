6eb29dadf544273d946f2fce7cac302a13952599
RSoph <kate.brender@gmail.com>
2017-03-12 20:43:58 -0400

    home controller seems like a good convention to follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  get '/jokes' => 'joke#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
