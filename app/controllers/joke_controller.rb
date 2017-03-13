589565009c3b2286589b8f3e18b314518384e5a5
RSoph <kate.brender@gmail.com>
2017-03-12 20:21:35 -0400

    joke controller

---
+++ app/controllers/joke_controller.rb
@@ -0,0 +1,41 @@
+class JokeController < ApplicationController
+
+	def index
+
+		@setup = [ "A broken pencil",
+					"Cows go"
+				 ]
+		@punchline = 	[ "Nevermind, it's pointless",
+						  "No silly, cows go 'moo'!"
+						]
+
+		message_body = params["Body"]
+		message_body ||= "just testing"
+		body = message_body.downcase.strip
+		from_number = params["From"]
+		from_number ||= '7187535492'
+
+		def message(to_number, text_body)
+			client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+			message = client.messages.create(
+			from: "+15675234372",
+			to: to_number,
+			body: text_body
+			)
+		end
+
+		if body == 'joke'
+			message(from_number, "Knock knock!")
+	  	elsif body == "who's there?" || body == "whos there?" || body == "whos there"
+			message(from_number, @setup[0])
+		elsif body == @setup[0] + " who?"
+			message(from_number, @punchline[0])
+		render nothing: true
+	end
+
+	private
+
+	def text_params
+      params.permit(:message_body, :from_number)
+    end
+end
